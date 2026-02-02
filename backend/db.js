28f3fad5a0c11b5aa0c4cc679880d74886c5b11b
ysodki-maker <ysodki@virtualconnect.ma>
2026-02-02 10:49:11 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "finitions_uploads_db"
+  host: "127.0.0.1",
+  user: "u180391690_crm_core",
+  password: "VOgqp;b9",
+  database: "u180391690_crm_core"
 });
 
 db.connect(err => {
@@ -13,3 +13,4 @@ db.connect(err => {
 });
 
 module.exports = db;
+

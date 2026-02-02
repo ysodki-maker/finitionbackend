63e67b2584dd895b2325bb508a5701ae70370b9e
ysodki-maker <ysodki@virtualconnect.ma>
2026-02-02 14:56:33 +0100

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://lightcyan-nightingale-747331.hostingersite.com", // ton backend Express
+  baseURL: "https://corecrm.cosinus.ma", // ton backend Express
 });
+
 
